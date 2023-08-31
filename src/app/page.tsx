f227f38adcc4e32a8ad5eb0f567df17dd19fbb21
rajahn01 <rafayuno@gmail.com>
2023-08-31 16:13:30 -0300

    jest configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen flex flex-row">
-      <div>Hello World 1</div>
+      <h1> Hello World</h1>
       <div>Hello World 2</div>
       <div>Hello World 3</div>
     </div>
